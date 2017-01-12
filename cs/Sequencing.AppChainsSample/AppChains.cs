b787d9033a289af9a124ff37c9f41440cd281cd0
Anna Derkach <aderkach@plexteq.com>
2017-01-12 17:42:39 +0200

    AC-16 combined.cs file into one AppChains.cs

--- cs/Sequencing.AppChainsSample/AppChains.cs
+++ cs/Sequencing.AppChainsSample/AppChains.cs
@@ -1,12 +1,20 @@
 using System;
 using System.Collections.Generic;
-using System.Globalization;
+using System.IO;
 using System.Linq;
+using System.Net;
+using System.Threading;
 using System.Threading.Tasks;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+using RestSharp;
 using Sequencing.AppChainsSample.SQAPI;
 
 namespace Sequencing.AppChainsSample
 {
+    /// <summary>
+    /// High level wrapper for accessing appchains functionality
+    /// </summary>
     /// <summary>
     /// High level wrapper for accessing appchains functionality
     /// </summary>
@@ -126,7 +134,7 @@ namespace Sequencing.AppChainsSample
             return GetRawReportImpl(_startApp);
         }
 
-        protected Dictionary<string, Report>  GetReportImplBatch(Dictionary<string, AppResultsHolder> resultHolder)
+        protected Dictionary<string, Report> GetReportImplBatch(Dictionary<string, AppResultsHolder> resultHolder)
         {
             var reportList = new Dictionary<string, Report>();
             var appChainsresult = GetRawReportImplBatch(resultHolder);
@@ -149,7 +157,7 @@ namespace Sequencing.AppChainsSample
                     List<long> noneCompletedJobs = new List<long>();
 
                     foreach (var result in appResult)
-                        if(result.Value.Status.Status != "Completed")
+                        if (result.Value.Status.Status != "Completed")
                             noneCompletedJobs.Add(result.Value.Status.IdJob);
 
                     if (noneCompletedJobs.Count == 0)
@@ -255,6 +263,460 @@ namespace Sequencing.AppChainsSample
         {
             var _uri = new Uri(backendFacade.ServiceUrl);
             return new Uri(_uri, string.Format("GetReportFile?idJob={0}", fileId));
+        }
+    }
+
+    /// <summary>
+    /// App results data holder
+    /// </summary>
+    public class AppResultsHolder
+    {
+        private readonly List<ItemDataValue> resultProps = new List<ItemDataValue>();
+
+        public List<ItemDataValue> ResultProps
+        {
+            get { return resultProps; }
+        }
+
+        /// <summary>
+        /// Status of application job
+        /// </summary>
+        public AppStatus Status { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("ResultProps: {0}, Status: {1}",
+                resultProps.Aggregate("", (s, value) => s + "," + value.ToString()), Status);
+        }
+    }
+
+    /// <summary>
+    /// Application start parameters
+    /// </summary>
+    public class AppStartParams
+    {
+        public AppStartParams()
+        {
+            Pars = new List<NewJobParameter>();
+        }
+
+        public string AppCode { get; set; }
+        public List<NewJobParameter> Pars { get; set; }
+    }
+
+    /// <summary>
+    /// AppStatus class describe the state of the executing app job
+    /// </summary>
+    public class AppStatus
+    {
+        public long IdJob { get; set; }
+        public string Status { get; set; }
+        public bool? CompletedSuccesfully { get; set; }
+        public DateTime? FinishDt { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("IdJob: {0}, Status: {1}, CompletedSuccesfully: {2}, FinishDt: {3}", IdJob, Status, CompletedSuccesfully, FinishDt);
+        }
+    }
+
+    /// <summary>
+    /// Access facade for SQAPI endpoint
+    /// </summary>
+    public class BackendServiceFacade
+    {
+        private int ATTEMPTS_COUNT = 30;
+        private int RETRY_TIMEOUT = 5000;
+        private readonly string token;
+        private readonly string serviceUrl;
+
+        public string ServiceUrl
+        {
+            get { return serviceUrl; }
+        }
+
+        public BackendServiceFacade(string token, string serviceUrl)
+        {
+            this.token = token;
+            this.serviceUrl = serviceUrl;
+        }
+
+        private T ExecuteRq<T>(RestRequest rq) where T : new()
+        {
+            var _cl = CreateClient();
+            for (int _idx = 0; _idx < ATTEMPTS_COUNT; _idx++)
+            {
+                var _execute = _cl.Execute<T>(rq);
+                if (_execute.StatusCode != HttpStatusCode.OK)
+                {
+                    Thread.Sleep(RETRY_TIMEOUT);
+                    continue;
+                }
+
+                return JsonConvert.DeserializeObject<T>(_execute.Content);
+            }
+            throw new Exception("Unable to call service, last response was:");
+        }
+
+        private Dictionary<string, AppResultsHolder> ExecuteRqExtended(RestRequest rq)
+        {
+            var _cl = CreateClient();
+            for (int _idx = 0; _idx < ATTEMPTS_COUNT; _idx++)
+            {
+                var _execute = _cl.Execute(rq);
+                if (_execute.StatusCode != HttpStatusCode.OK)
+                {
+                    Thread.Sleep(RETRY_TIMEOUT);
+                    continue;
+                }
+                return JArray.Parse(_execute.Content)
+                    .Select(x => x.ToObject<KeyValuePair<string, AppResultsHolder>>())
+                    .ToDictionary(x => x.Key, x => x.Value);
+            }
+            throw new Exception("Unable to call service, last response was:");
+        }
+
+        private RestRequest CreateRq(string opName, Method method)
+        {
+            var _restRequest = new RestRequest(opName, method);
+            return _restRequest;
+        }
+
+        private RestClient CreateClient()
+        {
+            var _restClient = new RestClient(serviceUrl);
+            _restClient.Authenticator = new OAuth2AuthorizationRequestHeaderAuthenticator(token, "Bearer");
+            return _restClient;
+        }
+
+        /// <summary>
+        /// Retrieves app results for previously executed appchain job
+        /// </summary>
+        /// <param name="idJob"></param>
+        /// <returns></returns>
+        public AppResultsHolder GetAppResults(long idJob)
+        {
+            var _restRequest = CreateRq("GetAppResults", Method.GET);
+            _restRequest.AddParameter("idJob", idJob, ParameterType.QueryString);
+            return ExecuteRq<AppResultsHolder>(_restRequest);
+        }
+
+        public Dictionary<string, AppResultsHolder> GetAppResultsBatch(dynamic idJob)
+        {
+            var _restRequest = CreateRq("GetAppResultsBatch", Method.POST);
+            _restRequest.AddParameter(new Parameter
+            {
+                Name = "application/json",
+                Value = JsonConvert.SerializeObject(idJob),
+                Type = ParameterType.RequestBody
+            });
+            return ExecuteRqExtended(_restRequest);
+        }
+
+        /// <summary>
+        /// Starts app chain execution
+        /// </summary>
+        /// <param name="pars">app parameters</param>
+        /// <returns>appchain job id</returns>
+        public AppResultsHolder StartApp(AppStartParams pars)
+        {
+            var _restRequest = CreateRq("StartApp", Method.POST);
+            _restRequest.AddParameter(new Parameter
+            {
+                Name = "application/json",
+                Value = JsonConvert.SerializeObject(pars),
+                Type = ParameterType.RequestBody
+            });
+            return ExecuteRq<AppResultsHolder>(_restRequest);
+        }
+
+        public Dictionary<string, AppResultsHolder> StartAppBatch(BatchAppStartParams pars)
+        {
+            var restRequest = CreateRq("StartAppBatch", Method.POST);
+            restRequest.AddParameter(new Parameter
+            {
+                Name = "application/json",
+                Value = JsonConvert.SerializeObject(pars),
+                Type = ParameterType.RequestBody
+            });
+            return ExecuteRqExtended(restRequest);
+        }
+
+        private IRestResponse ExecuteRestRq(RestRequest rq)
+        {
+            RestClient _restClient = CreateClient();
+            while (true)
+            {
+                int _attemptIdx = 0;
+                {
+                    IRestResponse _restResponse = _restClient.Execute(rq);
+                    if (_restResponse.StatusCode == HttpStatusCode.OK)
+                        return _restResponse;
+                    if (_attemptIdx < ATTEMPTS_COUNT)
+                    {
+                        Thread.Sleep(RETRY_TIMEOUT);
+                        _attemptIdx++;
+                    }
+                    else
+                        throw new Exception("Error while request to web-server:" + _restResponse.StatusCode +
+                                            Environment.NewLine + _restResponse.Content);
+                }
+            }
+        }
+    }
+
+    /// <summary>
+    /// Access facade for beacon endpoints
+    /// </summary>
+    public class BeaconFacade
+    {
+        private int ATTEMPTS_COUNT = 30;
+        private int RETRY_TIMEOUT = 5000;
+        private readonly string serviceUrl;
+
+        public BeaconFacade(string serviceUrl)
+        {
+            this.serviceUrl = serviceUrl;
+        }
+
+        private string ExecuteRq(RestRequest rq)
+        {
+            var _cl = CreateClient();
+            for (int _idx = 0; _idx < ATTEMPTS_COUNT; _idx++)
+            {
+                var _execute = _cl.Execute(rq);
+                if (_execute.StatusCode != HttpStatusCode.OK)
+                {
+                    Thread.Sleep(RETRY_TIMEOUT);
+                    continue;
+                }
+                return _execute.Content;
+            }
+            throw new Exception("Unable to call service, last response was:");
+        }
+
+        private RestRequest CreateRq(string opName, Method method)
+        {
+            var _restRequest = new RestRequest(opName, method);
+            return _restRequest;
+        }
+
+        private RestClient CreateClient()
+        {
+            var _restClient = new RestClient(serviceUrl);
+            return _restClient;
+        }
+
+        public string GetSequencingBeacon(int chrom, int pos, string allele)
+        {
+            var _restRequest = CreateRq("SequencingBeacon", Method.GET);
+            _restRequest.AddParameter("chrom", chrom, ParameterType.QueryString);
+            _restRequest.AddParameter("pos", pos, ParameterType.QueryString);
+            _restRequest.AddParameter("allele", allele, ParameterType.QueryString);
+            return ExecuteRq(_restRequest);
+        }
+
+        public string GetPublicBeacon(int chrom, int pos, string allele)
+        {
+            var _restRequest = CreateRq("PublicBeacons", Method.GET);
+            _restRequest.AddParameter("chrom", chrom, ParameterType.QueryString);
+            _restRequest.AddParameter("pos", pos, ParameterType.QueryString);
+            _restRequest.AddParameter("allele", allele, ParameterType.QueryString);
+            return ExecuteRq(_restRequest);
+        }
+    }
+
+    /// <summary>
+    /// Represents result property value
+    /// </summary>
+    public class ItemDataValue
+    {
+        public string Name { get; set; }
+        public string Title { get; set; }
+        public string SubTitle { get; set; }
+        public string Description { get; set; }
+        public string Type { get; set; }
+        public string SubType { get; set; }
+        public string Value { get; set; }
+
+        public override string ToString()
+        {
+            return string.Format("Name: {0}, Title: {1}, SubTitle: {2}, Description: {3}, Type: {4}, SubType: {5}, Value: {6}", Name, Title, SubTitle, Description, Type, SubType, Value);
+        }
+    }
+
+
+    /// <summary>
+    /// Appchain job parameter holder
+    /// </summary>
+    public class NewJobParameter
+    {
+        public NewJobParameter(string name, string value)
+        {
+            Name = name;
+            Value = value;
+        }
+
+        public NewJobParameter(string value)
+        {
+            Value = value;
+        }
+
+        public NewJobParameter(long? val)
+        {
+            ValueLong = val;
+        }
+
+
+        public NewJobParameter()
+        {
+        }
+
+        public string Name { get; set; }
+        public string Value { get; set; }
+        public long? ValueLong { get; set; }
+    }
+
+    /// <summary>
+    /// Appchain execution response
+    /// </summary>
+    public class StartAppRs
+    {
+        public long jobId { get; set; }
+    }
+
+    public class BatchAppStartParams
+    {
+        public BatchAppStartParams()
+        {
+            Pars = new List<AppStartParams>();
+        }
+        public List<AppStartParams> Pars { get; set; }
+    }
+
+    /// <summary>
+    ///  Class that represents result entity if it's file	
+    /// </summary>
+    public class FileResultValue : ResultValue
+    {
+        private readonly string name;
+        private readonly string extension;
+        private readonly Uri url;
+
+        public FileResultValue(string name, string extension, Uri url) : base(ResultType.FILE)
+        {
+            this.name = name;
+            this.extension = extension;
+            this.url = url;
+        }
+
+        public string Name
+        {
+            get { return name; }
+        }
+        
+        public Uri Url
+        {
+            get { return url; }
+        }
+
+        public void saveTo(string token, string fullPathWithName)
+        {
+            var path = Path.Combine(fullPathWithName, name);
+            new SqApiWebClient(token).DownloadFile(url, path);
+        }
+
+        public string getExtension()
+        {
+            return extension;
+        }
+    }
+
+    /// <summary>
+    /// Report class represents report available to the end client
+    /// </summary>
+    public class Report
+    {
+        public bool Succeeded { get; set; }
+        private List<Result> results;
+
+        public List<Result> getResults()
+        {
+            return results;
+        }
+
+        public void setResults(List<Result> results)
+        {
+            this.results = results;
+        }
+    }
+
+    /// <summary>
+    /// Class that represents single report result entity
+    /// </summary>
+    public class Result
+    {
+        private ResultValue value;
+        private String name;
+
+        public Result(String name, ResultValue resultValue)
+        {
+            this.name = name;
+            this.value = resultValue;
+        }
+
+        public ResultValue getValue()
+        {
+            return value;
+        }
+
+        public String getName()
+        {
+            return name;
+        }
+    }
+
+    /// <summary>
+    /// Enumerates possible result entity types
+    /// </summary>
+    public enum ResultType
+    {
+        FILE,
+        TEXT
+    }
+    /// <summary>
+    /// Base class for result values
+    /// </summary>
+    public class ResultValue
+    {
+        private ResultType type;
+
+        public ResultValue(ResultType type)
+        {
+            this.type = type;
+        }
+
+        public ResultType getType()
+        {
+            return type;
+        }
+    }
+
+    /// <summary>
+    /// Class that represents result entity if plain text string
+    /// </summary>
+    public class TextResultValue : ResultValue
+    {
+        private string data;
+
+        public TextResultValue(string data) : base(ResultType.TEXT)
+        {
+            this.data = data;
+        }
+
+        public string Data
+        {
+            get { return data; }
         }
     }
 }
